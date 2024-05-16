3764937c24680b9cf49e2d2224bebc6022836d59
Asakippam-Simon <simonasakipaam@gmail.com>
2024-05-16 00:14:49 +0000

    Adding file

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -22,3 +22,8 @@ def get_page(url: str) -> str:
     result = requests.get(url).content.decode('utf-8')
     redis_store.setex(res_key, timedelta(seconds=10), result)
     return result
+'''Test the function
+'''
+if __name__ == "__main__":
+    url = "http://slowwly.robertomurray.co.uk/delay/10000/url/https://www.example.com"
+    print(get_page(url))
